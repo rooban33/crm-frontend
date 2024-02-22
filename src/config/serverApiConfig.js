e4df4e96d4ab61119e21378fc9e7ff14bc419702
rooban33 <shajithgunasekaran@gmail.com>
2024-02-22 17:17:48 +0530

    hchange

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:3002/api/';
+    : 'https://crm-backend-vuge.onrender.com/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
