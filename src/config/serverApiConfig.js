78aa687ff7098eb13a003a8b1ea997c9b2a23cc2
rooban33 <shajithgunasekaran@gmail.com>
2024-02-22 17:18:44 +0530

    hchange1

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -5,11 +5,11 @@ export const API_BASE_URL =
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:3002/';
+    : 'https://crm-backend-vuge.onrender.com/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:3002/download/';
+    : 'https://crm-backend-vuge.onrender.com/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 //  console.log(
