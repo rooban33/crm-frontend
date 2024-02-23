41c5b445573394c5e8b1171b05cecb3396d87554
rooban33 <shajithgunasekaran@gmail.com>
2024-02-23 16:55:09 +0530

    ver

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,15 +1,15 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'https://crm-backend-vuge.onrender.com/api/';
+    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'https://crm-backend-vuge.onrender.com/';
+    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'https://crm-backend-vuge.onrender.com/download/';
+    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 //  console.log(
