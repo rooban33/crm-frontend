5bd040141847caad65c4e33aa0630a3314461ced
rooban33 <shajithgunasekaran@gmail.com>
2024-02-23 17:04:47 +0530

    ver2

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,15 +1,15 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/api/';
+    : 'https://crm-backend-lime.vercel.app/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/';
+    : 'https://crm-backend-lime.vercel.app/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'https://c3cd-2401-4900-7b8d-4f5-25da-6c66-339d-75f.ngrok-free.app/download/';
+    : 'https://crm-backend-lime.vercel.app/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 //  console.log(
