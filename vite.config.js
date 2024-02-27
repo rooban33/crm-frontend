e26b256a9c690bf5af3e4944d8e2a49011912b78
Tarun <143323486+Tarun0008@users.noreply.github.com>
2024-02-27 19:05:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : 'https://crm-backend-lime.vercel.app/;
 
   const config = {
     plugins: [react()],
